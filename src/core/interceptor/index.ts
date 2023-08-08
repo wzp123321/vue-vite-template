698d1006babc8f8ec4333eaa152dd501c70cb804
wanzp <wanzp@tiansu-china.com>
2023-08-08 15:36:45 +0800

    新增拦截器todo注释

--- src/core/interceptor/index.ts
+++ src/core/interceptor/index.ts
@@ -2,12 +2,14 @@
  * @Author: wanzp
  * @Date: 2023-08-07 22:25:42
  * @LastEditors: wanzp
- * @LastEditTime: 2023-08-08 14:20:13
+ * @LastEditTime: 2023-08-08 15:36:25
  * @Description: 请求拦截器
  */
 import axios, { InternalAxiosRequestConfig, AxiosResponse } from 'axios';
 import { Common_IRequestConfig } from './api';
 import { addHeader, addPrefix, addTimeOut, checkAxiosPermission, checkBlobPermission } from './utils';
+
+// Todo----------请求取消？？Map
 
 //  请求拦截器
 axios.interceptors.request.use(
